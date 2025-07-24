3c4c1420c42d3d50d761e9d7c9ece31908dd8644
MZBA201 <akbar.zain@yahoo.com>
2025-07-24 10:25:07 +0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Zain_Akbar',
+    author_email='akbar.zain@yahoo.com',
+    pakages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
+
